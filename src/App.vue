f020410ad5b26fc703f71eac66bc9fc02007336a
Czcibor Bohusz-Dobosz <czbd@o2.pl>
2024-08-30 09:58:51 +0200

    Already looking better

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 	import { ref, watch } from "vue";
+	import Headerro from "./components/Headerro.vue";
 	import Selectorro from "./components/Selectorro.vue";
 	const test = ref("test");
 	const keywords = [
@@ -10,14 +11,18 @@
 </script>
 
 <template>
-	<header>
-		
-	</header>
-	<main>
-		{{  test }}
-		<Selectorro :options="keywords"/>
-	</main>
-	<footer>
-		
-	</footer>
+	<div class="bg-zinc-100 grid grid-cols-1 min-h-lvh justify-items-center items-center">
+		<div class="w-11/12 lg:w-1/2">
+			<Headerro/>
+			<main>
+				<p class="py-2 pl-1">
+					Skills:
+				</p>
+				<Selectorro  :options="keywords"/>
+			</main>
+			<footer class="text-center text-zinc-500 py-10">
+				&copy; 2024 czbd
+			</footer>
+		</div>
+	</div>
 </template>
---
+++ src/components/Headerro.vue
@@ -0,0 +1,31 @@
+<template>
+    <header class="flex flex-col justify-center py-5">
+        <h1
+            class="
+                text-4xl
+                hover:text-5xl
+                object-contain
+                hover:font-black
+                italic
+                text-transparent
+                bg-clip-text
+                bg-zinc-900
+                hover:bg-gradient-to-r
+                from-zinc-900
+                to-red-500
+            "
+        ></h1>
+    </header>
+</template>
+
+<style>
+    h1:before {
+        content: "Please, kindly, fill out a list of your skills for the hiring process.";
+    }
+    h1:hover:before {
+        content: "TELL YO SKILL";
+    }
+    h1:hover {
+        text-align: center;
+    }
+</style>
